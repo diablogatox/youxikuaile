9cb31c7d463481424655a33a705d889a364fc11b
diablogatox <diablogatox@gmail.com>
2015-04-08 13:48:45 +0800

    test

--- src/com/orfid/youxikuaile/FriendHomeActivity.java
+++ src/com/orfid/youxikuaile/FriendHomeActivity.java
@@ -1,8 +1,10 @@
 package com.orfid.youxikuaile;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.http.Header;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,11 +33,12 @@ public class FriendHomeActivity extends Activity implements View.OnClickListener
 
     private String uid;
     private ProgressBar mPbar;
-    private ImageView userPhoto, followActionHintIv;
+    private ImageView userPhoto, followActionHintIv, feedItemFileIv, gameItemIconIv;
     private TextView titleTv, uidTv, collegeTv, 
-    	gendernageTv, followActionHintTv;
+    	gendernageTv, followActionHintTv, fansCountTv, distanceInfoTv,
+    	feedItemTextTv, sitterDescTv, gameItemNameTv;
     private ImageButton backBtn;
-    private View followBtnView, chatBtnView;
+    private View followBtnView, chatBtnView, feedItemLl, gameItemLl;
     private boolean isFollowed;
 
     @Override
@@ -70,6 +73,16 @@ public class FriendHomeActivity extends Activity implements View.OnClickListener
         chatBtnView = findViewById(R.id.btn_chat);
         followActionHintTv = (TextView) findViewById(R.id.follow_action_hint_tv);
         followActionHintIv = (ImageView) findViewById(R.id.follow_action_hint_iv);
+        fansCountTv = (TextView) findViewById(R.id.fans_count_tv);
+        distanceInfoTv = (TextView) findViewById(R.id.distance_info_tv);
+        feedItemLl = findViewById(R.id.feed_item_ll);
+        feedItemTextTv = (TextView) findViewById(R.id.feed_item_text_tv);
+        feedItemFileIv = (ImageView) findViewById(R.id.feed_item_file_iv);
+        sitterDescTv = (TextView) findViewById(R.id.sitter_desc_tv);
+        gameItemLl = findViewById(R.id.game_item_ll);
+        gameItemIconIv = (ImageView) findViewById(R.id.game_item_icon_iv);
+        gameItemNameTv = (TextView) findViewById(R.id.game_item_name_tv);
+        
 
         backBtn.setOnClickListener(this);
         followBtnView.setOnClickListener(this);
@@ -284,7 +297,54 @@ public class FriendHomeActivity extends Activity implements View.OnClickListener
                 try {
                     int status = response.getInt("status");
                     if (status == 1) { // success
-                    	
+                    	JSONObject data = response.getJSONObject("data");
+                    	JSONObject user = data.getJSONObject("user");
+                    	isFollowed = data.getBoolean("isFollow");
+                    	String school = user.isNull("school")?"":user.getString("school");
+                    	collegeTv.setText(school);
+                    	String birthday = user.has("birthday") && user.isNull("birthday")?null:user.getString("birthday");
+                    	Log.d("birthday=====>", birthday);
+                    	if (birthday != null) {
+                    		String age = Utils.getAge(Long.parseLong(birthday) * 1000)+"";
+                    		gendernageTv.setText(age);
+                    	}
+                    	int sex;
+                    	sex = user.isNull("sex")?2:user.getInt("sex");
+                    	if (sex == 1) {
+                    		gendernageTv.setCompoundDrawablesWithIntrinsicBounds( R.drawable.icon_man, 0, 0, 0);
+                    	}
+                    	fansCountTv.setText(user.getString("fans"));
+                    	if (!data.getString("distance").equals("未知距离") && !data.isNull("distance") && !data.getString("distance").equals("-1")) {
+                    		distanceInfoTv.setText(data.getString("distance") + " | 1小时前");
+                    		distanceInfoTv.setVisibility(View.VISIBLE);
+                    	}
+                    	if (!data.isNull("feed")) {
+                    		JSONObject jFeed = data.getJSONObject("feed");
+                    		if (jFeed.has("files")) {
+                    			if (!jFeed.isNull("files")) {
+                    				JSONArray files = jFeed.getJSONArray("files");
+                    				JSONObject jObj = files.getJSONObject(0);
+                    				String url = jObj.getString("url");
+                    				ImageLoader.getInstance().displayImage(url, feedItemFileIv);
+                    			}
+                    		}
+                    		String text = jFeed.getString("text");
+                    		feedItemTextTv.setText(text);
+                    		feedItemLl.setVisibility(View.VISIBLE);
+                    	}
+                    	if (!data.isNull("peiwan")) {
+                    		JSONObject jSitter = data.getJSONObject("peiwan");
+                    		sitterDescTv.setText(jSitter.getString("desc"));
+                    	}
+                    	if (!data.isNull("games")) {
+                    		JSONArray games = data.getJSONArray("games");
+                    		JSONObject jObj = games.getJSONObject(0);
+                    		String img = jObj.isNull("img")?null:jObj.getString("img");
+                    		String name = jObj.getString("name");
+                    		if (img != null) ImageLoader.getInstance().displayImage(img, gameItemIconIv);
+                    		gameItemNameTv.setText(name);
+                    		gameItemLl.setVisibility(View.VISIBLE);
+                    	}
                     } else if (status == 0) {
                         Toast.makeText(FriendHomeActivity.this, response.getString("text"), Toast.LENGTH_SHORT).show();
                     }
